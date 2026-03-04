dffb980414fccd07dd21dd38b034fa4a1746e4a5
minhkhoa <minhkhoact138@icloud.com>
2026-03-04 17:28:26 +0700

    Refactor RegistrationDAO for improved database handling

--- src/java/pe/model/registration/RegistrationDAO.java
+++ src/java/pe/model/registration/RegistrationDAO.java
@@ -18,128 +18,143 @@ import pe.utils.DbUtils;
  * @author ADMIN
  */
 public class RegistrationDAO implements Serializable {
-    
-    public boolean checkLogin(String username, String password)
-            throws SQLException, ClassNotFoundException {
+
+    public boolean checkLogin(String username, String password) throws SQLException, ClassNotFoundException {
         boolean result = false;
-        //1. model connect database
         Connection con = null;
         PreparedStatement stm = null;
         ResultSet rs = null;
         try {
-            //2. Create SQL String
+            // 1. Model connect DB
             con = DbUtils.getConnection();
-            if (con != null) {
-                String sql = "Select isAdmin "
-                        + "From Registration "
-                        + "Where username= ? and password = ?";
-                
-                //3. Create Statement Object
-                stm = con.prepareStatement(sql);
-                stm.setString(1, username);
-                stm.setString(2, password);
-                
-                //4. Execute Statement Object
-                rs = stm.executeQuery();
-                
-                //5. Process Result  
-                if(rs.next()){
-                    // if user has isAdmin in DB is 1 => accept login else not login
-//                    boolean role = rs.getBoolean("isAdmin");
-//                    if(role == true){
-//                        result = true;
-//                    }
-                    result = true;
-                }
+                if(con!= null) {
+            // 2. Model sẽ manipulates DB
+            // 2.1 tạo SQL String ( 1. ' a ' cụ thể ( ví dụ a) : statement : load - parse - excute . 2. ? : place holder : chờ :  ( câu lệnh có dạng preparedStatement ) 
+            // load - parse - receive - excute . vị trí dấu chấm  ? bắt đầu từ số 1 (từ trên xuống dưới, trái qua phải ) 
+            String sql = "select username "
+                    + "from Registration "
+                    + "where username = ? "
+                    + "And password = ? "
+                    + "And isAdmin = 1 ";
+            // 2.2 Tạo Statement Object ( nạp dữ liệu vào bộ nhớ ) 
+            stm = con.prepareStatement(sql);
+            stm.setString(1, username);
+            stm.setString(2, password);
+            // 2.3 Excute Query
+            rs = stm.executeQuery();
+            // 3. Model Xử lí Result
+            if (rs.next()) { // buoc 16
+                result = true; // buoc 17
             }
+                } // connection is available
         } finally {
-            /*Luu y:   
-                + dung xong phai dong lai
-                + Dong nguoc voi luc mo
-            */
-            if (rs != null) {
+            if ( rs!=null ) {
                 rs.close();
             }
-            if (stm != null) {
+            if (stm!= null) {
                 stm.close();
             }
             if (con != null) {
                 con.close();
             }
         }
+
         return result;
     }
-
-    // Tao danh sach de luu account
+    
     private List<RegistrationDTO> accounts;
 
     public List<RegistrationDTO> getAccounts() {
         return accounts;
     }
-
-    public void searchLastname(String searchValue)
-            throws SQLException, ClassNotFoundException {
+    
+    public void searchLastname (String searchValue) throws SQLException, ClassNotFoundException {
         Connection con = null;
         PreparedStatement stm = null;
         ResultSet rs = null;
         try {
-            //1. model connect database
+            // 1. Model connect DB
             con = DbUtils.getConnection();
-            if (con != null) {
-                /* 2. Create SQL String
-                    + moi menh de SQL phai viet tren 1 dong
-                    + moi khi xuong dong bat buoc phai them khoang trang
-                 */
-                String sql = "Select username, password, lastname, isAdmin "
-                        + "From Registration "
-                        + "Where lastname like ?";
-
-                //3. Create Statement Object
-                stm = con.prepareStatement(sql);
-                stm.setString(1, "%" + searchValue + "%"); //tham so thu nhat la value duoc chuyen vao searchLastname
-
-                /*4. Execute Statement Object 
-                    CUD--> so int the hien so dong affected 
-                    row|R--> result set: 1 object tro den 1 list, moi phan tu trong list 
-                    se mapping vs 1 dong trong server, phan tu dau tien chua BeginOfFile, 
-                    va cuoi cung chua EndOfFile, de dich chuyen con tro su dung phuong 
-                    thuc next() tra ve T or F co dac tinh la forward only */
-                rs = stm.executeQuery();
-
-                /*5. Process Result
-                    phai bt cau lenh tra 1 row hay nhiu row:
-                        + only 1 -> use if,
-                        + more than 1 -> use while */
+                if(con!= null) {
+            // 2. Model sẽ manipulates DB
+            // 2.1 tạo SQL String 
+            String sql = "SELECT username, password, lastname, isAdmin "
+                           + "FROM Registration "
+                           + "WHERE lastname LIKE ?";
+            // 2.2 Tạo Statement Object ( nạp dữ liệu vào bộ nhớ ) 
+            stm = con.prepareStatement(sql);
+            stm.setString(1, "%" + searchValue + "%");
+            // 2.3 Excute Query
+            rs = stm.executeQuery();
+            // 3. Model Xử lí Result
                 while (rs.next()) {
-                    //5.1 Model loads/get data from resultset
+                    // 3.1 model gets dữ liệu từ Result set
                     String username = rs.getString("username");
                     String password = rs.getString("password");
                     String fullName = rs.getString("lastname");
                     boolean role = rs.getBoolean("isAdmin");
-
-                    //5.2 Model sets data to DTO object
-                    RegistrationDTO dto = new RegistrationDTO(username, password, fullName, role);
+                    // 3.2 Model sets dữ liệu từ DTO object
+                    RegistrationDTO dto = new RegistrationDTO
+                        (username, password, fullName, role);
                     if (this.accounts == null) {
                         this.accounts = new ArrayList<>();
-                    } // account is invailuable
+                    } // account is unavailable
                     this.accounts.add(dto);
-                }//end traverse is available
-            }// connection is available
+                } // traverse each row in table
+                } // connection is available
         } finally {
-            /*Luu y:   
-                + dung xong phai dong lai
-                + Dong nguoc voi luc mo
-             */
-            if (rs != null) {
+            if ( rs!=null ) {
                 rs.close();
             }
+            if (stm!= null) {
+                stm.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+        
+    }
+    
+    public boolean deleteAccount(String username) throw SQLException, ClassNotFoundException {
+        
+    }
+    
+    public boolean createAccount(RegistrationDTO account) throw SQLException, ClassNotFoundException {
+        boolean result = false;
+        Connection con = null;
+        PreparedStatement stm = null;
+        
+        try {
+            con = DbUtils.getConnection();
+            if (con != null) {
+                String sql = "Insert Into Registration("
+                        + "username, password, lastName, isAdmin"
+                        + ") Value("
+                        + "?, ?, ?, ?"
+                        + ")";
+                stm = con.prepareStatement(sql);
+                stm.setString(1, account.getUsername());
+                stm.setString(2, account.getPassword());
+                stm.setString(3, account.getFullName());
+                stm.setString(4, account.isAdmin());
+                
+                int effectRows = stm.executeUpdate();
+                
+                if (effectRows > 0) {
+                    result = true;
+                }
 
+            }
+        } finally {
+            if (result != null) {
+                result.close();
+            }
             if (stm != null) {
                 stm.close();
             }
             if (con != null) {
                 con.close();
-
             }
         }
     }
